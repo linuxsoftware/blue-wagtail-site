f03230548aa593699a551e0dd14986a74d32e6b1
David <djm@linuxsoftware.co.nz>
2015-09-21 06:59:19 +1200

    8110

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cms.settings")
 
     from django.core.management.commands import runserver
-    runserver.DEFAULT_PORT = "8000"
+    runserver.DEFAULT_PORT = "8110"
 
     from django.core.management import execute_from_command_line
 
